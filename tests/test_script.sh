4a0361db375ea29df30c8bf4c5464f98e8537df6
pradsubedi <pradeep.subedi@rutgers.edu>
2020-02-13 16:24:09 -0500

    Updated Test Script

--- tests/test_script.sh
+++ tests/test_script.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
-./ndstore_server sm >&server.addr &
-sleep 2
-A=$(cat server.addr)
-./test_writer $A 1 3 1 1 1 1 4 4 4 8
-if [ $1 =eq 2 ]; then
+if [ $1 -eq 1 ]; then
+	./ndstore_server sm >&server.addr &
+	sleep 2
+	A=$(cat server.addr)
+	./test_writer $A 1 3 1 1 1 1 4 4 4 8
+elif [ $1 -eq 2 ]; then
+	./ndstore_server sm >&server.addr &
+	sleep 2
+	A=$(cat server.addr)
+	./test_writer $A 1 3 1 1 1 1 4 4 4 8
 	./test_reader $A 1 3 1 1 1 1 4 4 4 8
-elif [ $1 =eq 3 ]; then
+elif [ $1 -eq 3 ]; then
+	./ndstore_server sm >&server.addr &
+	sleep 2
+	A=$(cat server.addr)
+	./test_writer $A 1 3 1 1 1 1 4 4 4 8
 	./test_reader $A 1 3 1 1 1 1 4 2 4 8
-elif [ $1 =eq 4 ]; then
+elif [ $1 -eq 4 ]; then
+	./ndstore_server sm >&server.addr &
+	sleep 2
+	A=$(cat server.addr)
+	./test_writer $A 1 3 1 1 1 1 4 4 4 8
 	./test_reader $A 1 3 1 1 1 1 4 4 2 8
 fi
+kill $!
